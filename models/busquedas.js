62e6536a31e6b9a1b6ba6c1e735d3e33f620c1c7
Joao Ormeño <joao23.1996@gmail.com>
2022-04-11 22:16:30 -0400

    Modificaciones de codigo

--- models/busquedas.js
+++ models/busquedas.js
@@ -15,12 +15,12 @@ class Busquedas{
             //peticion http
             // console.log('ciudad',lugar);
             const resp = await axios.get('https://reqres.in/api/users?page=2');
-            console.log(resp.data.per_page);
+            console.log(resp.data);
 
             return [];
 
         } catch (error) {
-            return []
+            return [];
         }
 
         return []; //retornar los lugares
